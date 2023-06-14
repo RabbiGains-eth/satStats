45677eaa3336a629ef8fcfc76266effd99943a1e
RabbiGains.eth <127439651+RabbiGains-eth@users.noreply.github.com>
2023-06-13 21:07:23 -0400

    moved to react

--- scripts/background.js
+++
@@ -1,9 +0,0 @@
-chrome.runtime.onMessage.addListener(async function (request, sender, sendResponse) {
-  const tabId = sender.tab.id;
-
-  if (request.localstorage == "isAllowed") {
-    chrome.storage.local.get([request.localstorage]).then(function (result) {
-      chrome.tabs.sendMessage(tabId, { message: 'allowed', selected: result.isAllowed ? true : false });
-    });
-  }
-});
